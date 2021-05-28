dbcd914ecd6fddc2c841a153bb840520417164d2
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-05-28 10:51:39 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
-  
 /*
     jQuery for MaterializeCSS initialization
 */
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
 
 
@@ -18,4 +19,8 @@ $(document).ready(function () {
 // document.addEventListener('DOMContentLoaded', function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+//     let tooltips = document.querySelectorAll(".tooltipped");
+//     let tooltipsInstance = M.Tooltip.init(tooltips);
 // });
